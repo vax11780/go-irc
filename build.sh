d9f1dc1752e63ef454779f64ecf203247fa4bacd
Mike Dulaney <mdulaney@sigovs.com>
2013-07-15 15:15:05 -0400

    Attempting to ssh files to the test box.

--- build.sh
+++ build.sh
@@ -20,6 +20,25 @@ then
     exit 1   
 fi
 
-echo "Build completed successfully"
+echo "Build completed successfully, deploying to test box"
+
+scp bin/server ubuntu@ec2-54-227-84-172.compute-1.amazonaws.com
+
+if [ `whoami` = "jenkins" ]
+then
+    SSH_IDENTITY=~/.ssh/id_rsa
+else
+    SSH_IDENTITY=~/.ssh/virginia.pem
+fi
+
+#ssh -i $SSH_IDENTITY ubuntu@ec2-54-227-84-172.compute-1.amazonaws.com "server -d &"
+
+#if [ $? -ne 0 ]
+#then
+#    echo "SSH of server to test box failed, exiting"
+#    exit 1
+#fi
+
+echo "Deployment completed successfully"
 
 exit 0
