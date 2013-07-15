d0944e66574f12ce160c0007e78c41b9393cec52
Mike Dulaney <mdulaney@sigovs.com>
2013-07-15 14:54:43 -0400

    Successfully builds irc client and server

--- build.sh
+++ build.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
-echo "hello world"
+mkdir bin
+
+echo "Building client with go"
+go build -o bin/client client/client.go
+
+if [ $? -ne 0 ]
+then
+    echo "Client build failed, exiting"
+    exit 1   
+fi
+
+echo "Building server with go"
+go build -o bin/server server/server.go
+
+if [ $? -ne 0 ]
+then
+    echo "Server build failed, exiting"
+    exit 1   
+fi
+
+echo "Build completed successfully"
+
+exit 0
